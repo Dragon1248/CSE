1c2f93363c90b417cd3c2a66c346fc18f1049be0
xrac <31074769+Dragon1248@users.noreply.github.com>
2017-11-28 09:30:09 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,36 @@
+print("Hello world")
+
+# Sergio Yanez
+
+a = 4
+b = 3
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(3 ** 5)
+print(6 / 2)
+
+print()
+print("Try to figure this how this works")
+print(13 & 12)
+
+# the "%" sign is a modulus it finds the remainder
+
+car_name = "Wibe Mobile"
+car_type = "BMW"
+car_cylinders = 8
+car_mpg = 5000.9
+
+print("I have a car called %s. It's pretty nice." % car_name)
+print("I have a car called &s. It's a %."(car_name, car_type))  # watch the order
+
+# Here is where we get a little fancy
+print("what is your name?")
+name = input(">_")
+print("hello %s." % name)
+
+age = input (" How old are you?")
+print("%s?! That's really old. You belong in a retirement home." % age)
+
+
