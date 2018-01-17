8cb0828a48ae4ff8bc662968ace13e600622fd81
xrac <xrac@wetigers.org>
2018-01-17 09:41:43 -0800

    lucky 7s
    1-17-18

--- Lucky 7's.py
+++ Lucky 7's.py
@@ -1,7 +1,14 @@
 import random
-dicel = random.randint(1, 6)
+dice1 = random.randint(1, 6)
 dice2 = random.randint(1, 6)
-print(dicel)
+print(dice1)
 print(dice2)
-total + dice1 + dice2
-print
+money = 15
+total = dice1 + dice2
+print(dice1 + dice2)
+
+if total == 7:
+    money += 4
+else:
+    money -= 1
+print(money)
