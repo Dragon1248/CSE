fc938d881384a75c6a91a3911e08e4aa0794c668
xrac <xrac@wetigers.org>
2018-01-23 09:40:54 -0800

    hangman
    1-23-18

--- HangMan.SergioY.py
+++ HangMan.SergioY.py
@@ -1,13 +1,14 @@
 """
-This is a guid of how to
-make hangman
-
+A general guide for hangman
 1. Make a word bank - 10 items
 2. Select a random item to guess
-3. Hide the wordv(use * or )
-4. Reveals Letters based on input
-5. Create win and lose  Condition
+3. Take in a letter and add it to a list pf letters_guessed
+4. Hide and reveal letters
+5. Create the win Condition
+
 """
 
 word_banks = [ 'Phones', 'Tablets', 'Computer', 'Xbox One', 'Ps4','Xbox 360', 'Ps3', 'Fortnite', 'Pubg', 'Rules of survival' ]
 
+import string
+print(string)
