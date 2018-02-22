4862ddab5ef7c28146656bfaf2feb2ba4c2d2e07
xrac <xrac@wetigers.org>
2018-02-22 09:45:03 -0800

    2-22-18

--- ClassNotes.SergioY.py
+++ ClassNotes.SergioY.py
@@ -36,3 +36,39 @@ class car(object):
         self.HP = hp
         self.passengers = 0
         self.name = name
+        self.air_conditioning = True
+
+    def turn_on(self):
+        if self.running:
+            print("Nothing Happens.")
+        else:
+            self.running = True
+            print("The car starts.")
+
+    def move_forward(self):
+        if self.running:
+            print("You move forward")
+        else:
+            print("Nothing Happens")
+
+    def turn_off(self):
+        if self.running:
+            self.running = False
+            print("You turn the car off")
+        else:
+            print("The car is already off")
+
+    def go_for_drive(self, passengers):
+        print("%d passengers get in." % passengers)
+        self.passengers = passengers
+        self.turn_on()
+        self.move_forward()
+        self.move_forward()
+        self.move_forward()
+        self.turn_off()
+        print("%d passengers get out." % passengers)
+        self.passengers = 0
+
+
+my_car = Car("Maclaren P1", "Blue", 2, 10000000)
+
---
+++ SergioYanez.WorldMap OOP.py
@@ -0,0 +1,9 @@
+Class Room(object):
+
+    def __init__(self, desk, walls, doors, hp):
+        self.desk = desk
+        self.walls = 4
+        self.Windows = 2
+        self.Chairs = 35
+        self.Computers = 34
+        self.doors = 2
